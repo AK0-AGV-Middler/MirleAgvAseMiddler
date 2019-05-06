cf6c6bd21aed4faddd7b4779b871eb72964bf5f5
yiminglien@mirle.com.tw <yiminglien@mirle.com.tw>
2019-05-06 12:37:37 +0800

    Test Change

--- Mirle.AgvTests/Control/Handler/MainFlowHandlerTests.cs
+++ Mirle.AgvTests/Control/Handler/MainFlowHandlerTests.cs
@@ -5,41 +5,31 @@ using System.Collections.Generic;
 
 namespace Mirle.Agv.Control.Tests
 {
-    interface IInterfaceA
-    {
-        void DoSomething();
-    }
-
-    class ConcreteIIA : IInterfaceA
-    {
-        public void DoSomething()
-        {
-
-        }
-    }
-
-    abstract class ClassA
-    {
-        protected IInterfaceA interfaceA;
-
-        public ClassA(IInterfaceA interfaceA)
-        {
-            this.interfaceA = interfaceA;
-        }
-    }
-
-    class ClassB : ClassA
-    {
-        public int MemberB { get; set; }
-        public ClassB(IInterfaceA interfaceA) : base(interfaceA)
-        {
-        }
-    }
-
-
     [TestFixture()]
     public class MainFlowHandlerTests
     {
+        class AA
+        {
+            private BB bb;
+            public string a = "AAaa";
+        }
+
+        class BB
+        {
+            private AA aa;
+            public string b = "BBbb";
+
+            public BB(AA aa)
+            {
+                this.aa = aa;
+            }
+
+            public int HashAA()
+            {
+                return aa.GetHashCode();
+            }
+        }
+
         [Test()]
         public void MainFlowHandlerTest()
         {
@@ -52,39 +42,16 @@ namespace Mirle.Agv.Control.Tests
         [Test()]
         public void XXXTest()
         {
-            ConcreteIIA concreteIIA = new ConcreteIIA();
-            ClassB ins1 = new ClassB(concreteIIA);
-            ins1.MemberB = 123;
-            ClassB ins2 = new ClassB(concreteIIA);
-            ins2.MemberB = 456;
- 
-            List<ClassB> list001 = new List<ClassB>();
-            list001.Add(ins1);
-            list001.Add(ins2);
+            AA obA = new AA();
 
-            Assert.AreEqual(2, list001.Count);
+            BB obB = new BB(obA);
 
-            List<ClassB> list002 = list001;
+            var temp1 = obA.GetHashCode();
 
-            Assert.AreEqual(2, list002.Count);
-            Assert.AreEqual(123, list002[0].MemberB);
+            var temp2 = obB.HashAA();
 
-            List<ClassB> list003 = new List<ClassB>();
-            for (int i = 0; i < list001.Count; i++)
-            {
-                list003.Add(list001[i]);
-            }
-
-            Assert.AreEqual(2, list003.Count);
-            Assert.AreEqual(456, list003[1].MemberB);
-
-            list001.Clear();
-
-            Assert.AreEqual(0, list001.Count);
-
-            Assert.AreEqual(0, list002.Count);
-
-            Assert.AreEqual(2, list003.Count);
+            Console.Read();
+            
         }
     }
 }
